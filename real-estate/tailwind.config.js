e5f11a96c7bde5eddf1fd16b210ecdc505ac1a49
siva <sivakrishnaboya45@gmail.com>
2023-08-18 13:20:01 +0530

    real-estate final commit

---
+++ real-estate/tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+
+      boxShadow :{
+        "btn" : "0px 10px 15px rgba(1,115,153,0.2)"
+
+      },
+      fontSize : {
+      sm: '0.75rem',  //12px
+      base: '.875rem', //14px
+      md:'1rem',     //16px
+      lg : '1.25rem', //20px
+      xl: '1.5rem',  //24px
+      '2xl': '1.75rem', //28px
+      '3xl': '2.25rem', //36px
+      '4xl': '3rem' , //48px
+      '5xl' : '4rem' //64px
+      },
+
+      colors:{
+
+        blue : {
+          dark : "#0A72AD",
+
+        },
+        gray:{
+          dark : "#c4c4c4",
+          light : "#E5E5E5"
+
+        },
+        black : {
+          light : "#242527",
+          dark : "#2B2B2B",
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
